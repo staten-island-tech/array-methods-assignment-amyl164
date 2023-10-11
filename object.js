1a3973582983d317e8696ce129232c8de2d11b87
amyl164 <amyl81008@gmail.com>
2023-10-11 10:13:53 -0400

    afeafasc

--- object.js
+++ object.js
@@ -37,9 +37,13 @@ const teachers = [{name: 'Whalen', classes: ['AP Comp Sci', 'Freshman Comp Sci',
                   {name: 'Colangelo', classes: ["Physics", "Physics in Medicine", "Physics Lab"], hair: true, room: '144', college: 'idk harvard',},
                   {name: 'Carr', classes: ["Sophmore APWH", "Freshman APWH"], hair: true, room: '325', college: 'idk yale',},
                   {name: "callahan", classes: ["Sophmore Russian", "Junior Russian"], hair: true, room: '317', college: 'idk some irish school',},
-]
+];
+
+const result = classes.filter((classes) => classes.hair !==true)
+console.log(result);
+
 teachers.forEach((teacher)=> console.log(teacher.name));
-teachers.forEach((teacher)=> console.log(teacher.classes));
-teachers.forEach((teacher)=> console.log(teacher.hair));
-teachers.forEach((teacher)=> console.log(teacher.room));
-teachers.forEach((teacher)=> console.log(teacher.college));
+teachers.forEach((teacher)=> teacher.classes.forEach((classes) => console.log(classes)))
+ teachers.forEach((teacher)=> console.log(teacher.hair));
+ teachers.forEach((teacher)=> console.log(teacher.room));
+ teachers.forEach((teacher)=> console.log(teacher.college));
